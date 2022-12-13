74fbabc2bf0985ef74f4d6fbae4df311b1c8a2e3
SaiVyshnavi08 <saivyshnavigudipalli@gmail.com>
2022-12-13 21:09:07 +0530

    pizza

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,16 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+    @Override
+    public void addExtraCheese() {}
+
+    @Override
+    public void addExtraToppings() {}
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+         super.addExtraCheese();
+         super.addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,9 +6,22 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private boolean isCheeseAdded =false;
+    private boolean isVegToppingsAdded = false;
+    private boolean isNonVegToppingsAdded = false;
+    private boolean isBackPackOpted =false;
+
+    private int pizzaprice;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(this.isVeg){
+            this.price = 300;
+        }
+        else{
+            this.price =400;
+        }
+        pizzaprice = this.price;
     }
 
     public int getPrice(){
@@ -17,18 +30,46 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        isCheeseAdded = true;
+        this.price = this.price + 80;
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+        if(isVeg){
+            isVegToppingsAdded = true;
+            this.price = this.price+70;
+        }
+        else{
+            isNonVegToppingsAdded =true;
+            this.price = this.price +120;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        isBackPackOpted =true;
+        this.price = this.price +20;
     }
 
     public String getBill(){
         // your code goes here
+        String bill ="";
+       bill = bill + "Base Price Of The Pizza:"+ pizzaprice+ "\n";
+
+        if(isCheeseAdded){
+            bill = bill +"Extra Cheese Added: "+ 80 +"\n";
+        }
+        if(isVegToppingsAdded) {
+            bill = bill + "Extra Toppings Added:" + 70+"\n";
+        }
+        if(isNonVegToppingsAdded) {
+            bill = bill + "Extra Toppings Added:" + 120+"\n";
+        }
+        if(isBackPackOpted){
+            bill = bill + "Paperbag Added:" + 20+"\n";
+        }
         return this.bill;
     }
 }
