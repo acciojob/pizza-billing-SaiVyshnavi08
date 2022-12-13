275e7a91cc3552081f08d938b30c42a549457a93
SaiVyshnavi08 <saivyshnavigudipalli@gmail.com>
2022-12-13 22:13:24 +0530

    pizza1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,16 +2,12 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
-    @Override
-    public void addExtraCheese() {}
 
-    @Override
-    public void addExtraToppings() {}
 
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-         super.addExtraCheese();
-         super.addExtraToppings();
+         addExtraCheese();
+         addExtraToppings();
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,70 +6,81 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
-    private boolean isCheeseAdded =false;
-    private boolean isVegToppingsAdded = false;
-    private boolean isNonVegToppingsAdded = false;
-    private boolean isBackPackOpted =false;
+    private int cheesePrice =80;
+    private int ToppingsPrice;
+    private int TakeAwayPrice =20;
+
+
+    private boolean isCheeseAdded ;
+    private boolean isToppingsAdded ;
+    private boolean isBackPackOpted ;
+    private boolean  isBillGenerated;
 
     private int pizzaprice;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        if(this.isVeg){
+        this.isVeg= isVeg;
+        isCheeseAdded =false;
+        isToppingsAdded = false;
+         isBackPackOpted =false;
+         isBillGenerated = false;
+
+        if(isVeg==true){
             this.price = 300;
+            this.ToppingsPrice = 70;
         }
         else{
             this.price =400;
+            this.ToppingsPrice =120;
         }
-        pizzaprice = this.price;
+       bill = bill + "Base Price Of The Pizza:" + this.price +"\n";
     }
 
     public int getPrice(){
         return this.price;
     }
 
-    public void addExtraCheese(){
+    public void addExtraCheese() {
         // your code goes here
-        isCheeseAdded = true;
-        this.price = this.price + 80;
+        if (isCheeseAdded == false) {
+            this.price = this.price + this.cheesePrice;
+            isCheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
 
-        if(isVeg){
-            isVegToppingsAdded = true;
-            this.price = this.price+70;
-        }
-        else{
-            isNonVegToppingsAdded =true;
-            this.price = this.price +120;
+        if(isToppingsAdded == false){
+            this.price = this.price + this.ToppingsPrice;
+            isToppingsAdded = true;
         }
     }
 
-    public void addTakeaway(){
+    public void addTakeaway() {
         // your code goes here
-        isBackPackOpted =true;
-        this.price = this.price +20;
+        if (isBackPackOpted == false) {
+            this.price = this.price + TakeAwayPrice;
+            isBackPackOpted = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        String bill ="";
-       bill = bill + "Base Price Of The Pizza:"+ pizzaprice+ "\n";
+       if(isBillGenerated== false) {
+           isBillGenerated = true;
+           if (isCheeseAdded == true) {
+               bill = bill + "Extra Cheese Added: " + this.cheesePrice + "\n";
+           }
+           if (isToppingsAdded == true) {
+               bill = bill + "Extra Toppings Added:" + this.isToppingsAdded + "\n";
+           }
 
-        if(isCheeseAdded){
-            bill = bill +"Extra Cheese Added: "+ 80 +"\n";
-        }
-        if(isVegToppingsAdded) {
-            bill = bill + "Extra Toppings Added:" + 70+"\n";
-        }
-        if(isNonVegToppingsAdded) {
-            bill = bill + "Extra Toppings Added:" + 120+"\n";
-        }
-        if(isBackPackOpted){
-            bill = bill + "Paperbag Added:" + 20+"\n";
-        }
+           if (isBackPackOpted) {
+               bill = bill + "Paperbag Added:" + TakeAwayPrice + "\n";
+           }
+           bill = bill + "Total Price: " + this.price + "\n";
+       }
         return this.bill;
     }
 }
